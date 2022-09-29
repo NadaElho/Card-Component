7ec96ae3c9dbb84d37946339a877afd9b66210a1
Nada Elhosary <90730411+NadaElho@users.noreply.github.com>
2022-09-30 01:42:45 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let title= document.querySelector(".title");
-let advice= document.querySelector(".advice")
-fetch("https://api.adviceslip.com/advice").then((res)=>{
-    return res.json();
-}).then(data=>{
-    title.innerHTML=`Advice #${data.slip.id}`;
-    advice.innerHTML=`"${data.slip.advice}"`
-})
